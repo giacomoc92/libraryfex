89aec5a188d22820aba236e548810f81f55b34eb
giacomoc92 <consani.gia@gmail.com>
2023-02-08 18:06:47 +0100

    add header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+import HeaderComponent from './components/HeaderComponent';
 import ListAccountsComponent from './components/ListAccountsComponent';
 
 function App() {
   return (
     <div>
-      <div className='container'>
-        <ListAccountsComponent />
-      </div>
+      <HeaderComponent />
+        <div className='container'>
+          <ListAccountsComponent />
+        </div>
     </div>
   );
 }
